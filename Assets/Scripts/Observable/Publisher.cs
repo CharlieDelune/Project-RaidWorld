5c087373fb76a26c64f1d9ad15d96c52cfbb7bfe
Jeff Kapochus <jeffk@glidertech.com>
2020-02-23 11:49:02 -0500

    Prefab-ified enemies, Implemented Observers, Made Enemies Observers - This allows us to change the path WHILE BUILDING WALLS

---
+++ Assets/Scripts/Observable/Publisher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Publisher
+{
+    List<Observer> observers = new List<Observer>();
+
+    public void Notify(PublisherEvent ev)
+    {
+        foreach (Observer observer in observers)
+        {
+            observer.OnNotify(ev);
+        }
+    }
+
+    public void AddObserver(Observer observer)
+    {
+        observers.Add(observer);
+    }
+
+    public void RemoveObserver(Observer observer)
+    {
+        observers.Remove(observer);
+    }
+}
+
+public enum PublisherEvent
+{
+    TestNotify
+}
